84b9c9b8eeab94faa07ce6822e6de2c56b570ac3
DIOP <diopmo@ept.sn>
2020-10-08 15:37:29 +0000

    created models test

---
+++ src/test/java/io/work/confino/modelsTest/CandidateTest.java
@@ -0,0 +1,4 @@
+package io.work.confino.modelsTest;
+
+public class CandidateTest {
+}
---
+++ src/test/java/io/work/confino/utilsTest/VARIABLES.java
@@ -0,0 +1,51 @@
+package io.work.confino.utilsTest;
+
+import io.work.confino.models.Candidate;
+import io.work.confino.models.CompanyAccount;
+import io.work.confino.models.Job;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Variable {
+     final static List<Job> JOBS = new ArrayList<>(Arrays.asList(
+            Job.builder()
+                    .id("15ee")
+                    .name("Software Engineer")
+                    .typeOfContract("CDD")
+                    .build(),
+
+            Job.builder()
+                    .id("13op")
+                    .name("Backend Developer")
+                    .typeOfContract("CDI")
+                    .build()
+    ));
+     final static List<Candidate> CANDIDATES = new ArrayList<>(Arrays.asList(
+            Candidate.builder()
+                    .id("45er")
+                    .email("diopmo@ept.sn")
+                    .firstName("Mohamed")
+                    .lastName("DIOP")
+                    .build(),
+            Candidate.builder()
+                    .id("78er")
+                    .email("diopmo@ept.sn")
+                    .firstName("Karim")
+                    .lastName("SOW")
+                    .build()
+    ));
+     final static List<CompanyAccount> COMPANY_ACCOUNTS = new ArrayList<>(Arrays.asList(
+            CompanyAccount.builder()
+                    .id("155er")
+                    .username("diopmo")
+                    .password("gfllfd1556")
+                    .build(),
+            CompanyAccount.builder()
+                    .id("155er")
+                    .username("sowkarim")
+                    .password("thhhfhfff")
+                    .build()
+    ));
+}
